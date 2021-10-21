8817f265b5db7c87774b49571fce2e70623ee242
JuanTorres8718 <jdtorres8718@gmail.com>
2021-10-20 19:55:51 -0500

    se agrego vista de topbar y sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,93 @@
+import React from "react";
+import {
+  ArrowDropDown,
+  BusinessCenter,
+  Computer,
+  DataUsage,
+  Equalizer,
+  Home,
+  PeopleAlt,
+  PersonOutline,
+  Storage,
+} from "@material-ui/icons";
 import "./sidebar.scss";
+import { Link } from "react-router-dom";
 
 export default function Sidebar() {
-  return <div className="sidebar">creo que esto es una prueba</div>;
+  const hoverMenu = () => {
+    let options = document.getElementById("options");
+    options.classList.toggle("displayOptions")
+  };
+
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Menú Rápido</h3>
+          <ul className="sidebarList">
+            <Link to="/users" className="link">
+              <li className="sidebarListItem">
+                <PersonOutline className="sidebarIcon" />
+                Usuarios
+              </li>
+            </Link>
+            <Link to="/projects" className="link">
+              <li className="sidebarListItem">
+                <BusinessCenter className="sidebarIcon" />
+                Proyectos
+              </li>
+            </Link>
+            <Link to="/products" className="link">
+              <li className="sidebarListItem">
+                <Storage className="sidebarIcon" />
+                Productos
+              </li>
+            </Link>
+            <Link to="/talents" className="link">
+              <li className="sidebarListItem">
+                <PeopleAlt className="sidebarIcon" />
+                Talento Humano
+              </li>
+            </Link>
+            <Link to="/machinery" className="link">
+              <li className="sidebarListItem">
+                <Computer className="sidebarIcon" />
+                Maquinaria y equipo
+              </li>
+            </Link>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <Link to="/" className="link">
+              <li className="sidebarListItem">
+                <Home className="sidebarIcon" />
+                Inicio
+              </li>
+            </Link>
+            <li className="indicator" onClick={hoverMenu}>
+              <div className="ListIndicator">
+                <Equalizer className="sidebarIcon" />
+                Indicadores CTeI
+                <ArrowDropDown className="icon" />
+              </div>
+            </li>
+            <div id="options" className="options displayOptions">
+              <span>-Financiero</span>
+              <span>-Talento Humano</span>
+              <span>-Maquinaria y equipos</span>
+              <span>-Productos</span>
+            </div>
+            <Link to="/databank" className="link">
+              <li className="sidebarListItem">
+                <DataUsage className="sidebarIcon" />
+                Banco de datos
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,29 @@
 import "./topbar.scss";
+import { ArrowDropDown, Notifications } from "@material-ui/icons";
+import logo from "../../images/logo-sena.png";
 
 export default function Topbar() {
-  return <div className="topbar">barsito</div>;
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <img className="logo" src={logo} alt="logo-sena" />
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <Notifications className="iconNotifications" />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="profile">
+            <span>Pepito Fdez</span>
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Setting</span>
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
